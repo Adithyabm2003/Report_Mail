afafa8fc0ce546120d6d16c086226eafbd2a8d45
Adithya B M <103888807+Adithyabm2003@users.noreply.github.com>
2025-09-08 14:09:00 +0530

    Add files via upload

--- Splunk_Notification_3.py
+++ Splunk_Notification_3.py
@@ -28,7 +28,7 @@ def send_automated_emails(df, sender_email, sender_password, cc_email, log_conta
                 log_container.markdown(f"--- \n**Processing Site: {site_number}**")
 
                 # LOGIC 1: If anyone in a site has DOWNLOAD = YES, skip the entire site.
-                if 'YES' in site_df['DOWNLOAD'].unique():
+                if 'YES' in site_df['DOWNLOAD'].unique().upper():
                     log_container.warning(f"Site {site_number}: At least one user has downloaded. No emails will be sent.")
                     continue
 
